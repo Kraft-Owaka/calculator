d9dc4f86d31b7128bc23d1c017a3928545964d0e
kraft-owaka <owakakraft@gmail.com>
2019-10-25 13:40:14 +0300

    Added the multiplication and division functuion

--- js/javascript.js
+++ js/javascript.js
@@ -13,4 +13,19 @@ var number1 = parseInt(prompt("enter a number"));
 var number2 = parseInt(prompt("enter another number"));
 var result = subtract(number1, number2);
 alert(result);
-alert(subtract(number1, number2));
+
+var multiply = function(number1, number2) {
+return number1 -number2;
+}
+var number1 = parseInt(prompt("enter a number"));
+var number2 = parseInt(prompt("enter another number"));
+var result = multiply(number1, number2);
+alert(result);
+
+var divide = function(number1, number2) {
+return number1 -number2;
+}
+var number1 = parseInt(prompt("enter a number"));
+var number2 = parseInt(prompt("enter another number"));
+var result = divide(number1, number2);
+alert(result);
