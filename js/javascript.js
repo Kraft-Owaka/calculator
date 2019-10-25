06b4467031f558ce2ebbbb447a56eed24105074d
kraft-owaka <owakakraft@gmail.com>
2019-10-25 13:33:26 +0300

    Added a var about result to the add and subtract section

--- js/javascript.js
+++ js/javascript.js
@@ -1,13 +1,16 @@
 var add = function(number1, number2) {
     return number1 + number2;
-    }
-    var number1 = parseInt(prompt("Enter a number"));
-    var number2 = parseInt(prompt("Enter another number:"));
-    alert(add(number1, number2));
+}
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var result = add(number1, number2);
+alert(result);
 
 var subtract = function(number1, number2) {
-    return number1 -number2;
+return number1 -number2;
 }
 var number1 = parseInt(prompt("enter a number"));
-var number2 = parseInt(prompt("enter anoher number"));
+var number2 = parseInt(prompt("enter another number"));
+var result = subtract(number1, number2);
+alert(result);
 alert(subtract(number1, number2));
